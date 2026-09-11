2a0876a298aaddc55a6ce6377abd3cb8ce1056b1
fabinhose12 <echewfa@gmail.com>
2026-09-11 15:17:37 -0300

    exercicios feitos até a aula 21

---
+++ aula19/script.js
@@ -0,0 +1,40 @@
+// array = lista de coisas
+
+// index = posição do elemento dentro do array
+const lista = ['Luiz', 'Maria', 'João']; //strings
+
+
+console.log(lista instanceof Array); //verifica se a variavel é um array
+
+
+//array é considerado objeto, então podemos usar o typeof para verificar o tipo de dado
+
+console.log(typeof lista); // mostra o tipo de dado da variavel
+
+
+// console.log(lista.slice(0, -1)); mostra todos os elementos do array menos o ultimo
+
+
+
+
+// lista[0] = 'Pedro'; // substitui 'Luiz' por 'Pedro'
+
+// console.log(lista);
+
+// lista.push('Luiza'); // adiciona 'Luiza' no final do array
+// console.log(lista);
+
+// console.log(lista.length); // mostra o tamanho do array
+
+// lista.unshift('Fabio'); // adiciona 'Fabio' no inicio do array
+// console.log(lista); 
+
+// const removido = lista.pop(); // remove o ultimo elemento do array e retorna-o
+// console.log(removido);
+// console.log(lista);
+
+// console.log(lista.shift()); // remove o primeiro elemento do array e retorna-o
+// console.log(lista);
+
+// console.log(lista);
+// console.log(lista[2]); // João
---
+++ aula21/script.js
@@ -0,0 +1,16 @@
+// function saudacao(nome) {
+//    return `Olá, ${nome}!`;
+// } 
+
+
+// const variavel = saudacao('fabio'); // Olá, Fabio!
+// console.log(variavel); // undefined, pois a função não retorna nada
+
+function soma (x, y) {
+    const resultado = x + y;
+    return resultado; // retorna o valor da soma
+}
+
+console.log(soma(2, 2)); // 4
+
+//escopo da função
